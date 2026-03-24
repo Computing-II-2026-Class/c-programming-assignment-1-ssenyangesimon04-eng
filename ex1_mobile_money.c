cf8f9aa32b9c2477e83145feae04d57c465f0007
ssenyangesimon04-eng <ssenyangesimon04@gmail.com>
2026-03-24 20:50:01 +0300

    Update ex1_mobile_money.c

--- ex1_mobile_money.c
+++ ex1_mobile_money.c
@@ -1,11 +1,22 @@
-/* Name: Your Full Name */
-/* Student Number: Your Registration Number */
+/* Name: ssenyange simon */
+/* Student Number: 25/U/BIE/11579/PE */
 
 #include <stdio.h>
 
-int main(void)
-{
-    /* Write your code here */
+int main() {
+    float amount, fee_percent, fee, total;
+
+    printf("Enter amount to send: ");
+    scanf("%f", &amount);
+
+    printf("Enter transaction fee (%%): ");
+    scanf("%f", &fee_percent);
+
+    fee = (fee_percent / 100) * amount;
+    total = amount + fee;
+
+    printf("\nTransaction fee: %.2f UGX\n", fee);
+    printf("Total deducted: %.2f UGX\n", total);
 
     return 0;
 }
