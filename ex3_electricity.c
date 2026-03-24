a8de82bcfb4d54f91987debf2e8b78df96c905a1
ssenyangesimon04-eng <ssenyangesimon04@gmail.com>
2026-03-24 20:56:52 +0300

    Update ex3_electricity.c

--- ex3_electricity.c
+++ ex3_electricity.c
@@ -1,11 +1,22 @@
-/* Name: Your Full Name */
-/* Student Number: Your Registration Number */
-
+/* Name: ssenyange simon peter*/
+/* Student Number: 25/U/BIE/11579/PE */
 #include <stdio.h>
 
-int main(void)
-{
-    /* Write your code here */
+int main() {
+    int units;
+    float cost_per_unit, total_bill;
+
+    printf("Enter units consumed: ");
+    scanf("%d", &units);
+
+    printf("Enter cost per unit: ");
+    scanf("%f", &cost_per_unit);
+
+    total_bill = units * cost_per_unit;
+
+    printf("\nTotal bill: %.2f UGX\n", total_bill);
 
     return 0;
 }
+
+
