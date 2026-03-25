9955d0aa72963c96aa9cecfede809b5a76412ff6
ssenyangesimon04-eng <ssenyangesimon04@gmail.com>
2026-03-25 08:46:50 +0300

    Update ex2_fuel.c
    Assignment 2

--- ex2_fuel.c
+++ ex2_fuel.c
@@ -1,19 +1,21 @@
 /* Name:ssenyange simon */
 /* Student Number: 25/U/BIE/11579/PE */
 #include <stdio.h>
-
-int main() {
-    float distance, fuel, efficiency;
-
-    printf("Enter distance travelled (km): ");
+int main() 
+{
+    float distance;
+    float fuelused;
+    float efficiency;
+    printf("Enter distance (km): ");
     scanf("%f", &distance);
-
-    printf("Enter fuel used (litres): ");
-    scanf("%f", &fuel);
-
-    efficiency = distance / fuel;
-
-    printf("\nFuel efficiency: %.2f km/l\n", efficiency);
+    printf("Enter fuelused (litres): ");
+    scanf("%f", &fuelused);
+    if (fuelused != 0) {
+        efficiency = distance / fuelused;
+        printf("\nFuel efficiency: %.2f km/l\n", efficiency);
+    } else {
+        printf("\nError: Fuel used cannot be zero.\n");
+    }
 
     return 0;
 }
