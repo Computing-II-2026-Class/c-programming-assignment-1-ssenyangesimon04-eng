6c4fdc3285007afe8ed2e62d3b570f6dd2b959b4
ssenyangesimon04-eng <ssenyangesimon04@gmail.com>
2026-03-25 09:36:07 +0300

    Update ex3_electricity.c

--- ex3_electricity.c
+++ ex3_electricity.c
@@ -1,22 +1,16 @@
 /* Name: ssenyange simon peter*/
 /* Student Number: 25/U/BIE/11579/PE */
 #include <stdio.h>
-int main() 
-{
-    float distance;
-    float fuelused;
-    float efficiency;
-    printf("Enter distance (km): ");
-    scanf("%f", &distance);
-    printf("Enter fuelused (litres): ");
-    scanf("%f", &fuelused);
-    if (fuelused != 0) {
-        efficiency = distance / fuelused;
-        printf("\nFuel efficiency: %.2f km/l\n", efficiency);
-    } else {
-        printf("\nError: Fuel used cannot be zero.\n");
-    }
-
-    return 0;
+int main (){
+	float units;  // number of units
+	float cost; //cost per unit
+	float totalelectricitybill; //total electricity used
+	printf("enter units consumed:");
+	scanf("%f",&units);
+	printf("enter cost per unit");
+	scanf("%f",&cost);
+	totalelectricitybill =(units)*cost;
+	printf("total electricitybill is %.2f UGX",totalelectricitybill);
+	return 0;
 }
 
